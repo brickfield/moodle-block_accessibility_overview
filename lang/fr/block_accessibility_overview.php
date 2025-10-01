7f47b203f081ca400e9d573ceb131678fd18e5c1
Karen Holland <karen@brickfieldlabs.ie>
2025-10-01 16:24:06 +0000

    TW21394794 fr strings for overview

---
+++ lang/fr/block_accessibility_overview.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language pack for block_accessibility_overview
+ *
+ * @package     block_accessibility_overview
+ * @copyright   2024 Brickfield Education Labs, www.brickfield.ie
+ * @author      Sarah Chicheportich, created with Plugin Translator https://www.brickfield.ie/brickfield-translator/
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['accessibility_overview:addinstance'] = 'Ajouter un bloc Vue d\'ensemble de l\'accessibilité';
+$string['accessibility_overview:view'] = 'Voir le bloc Vue d\'ensemble de l\'accessibilité';
+$string['altformat'] = 'Conversion des contenus';
+$string['bookmeeting'] = 'Réserver un rendez-vous';
+$string['brickfieldenterprise'] = 'Plan Brickfield Entreprise';
+$string['brickfieldservices'] = 'Services Brickfield';
+$string['brickfieldstarter'] = 'Plan Brickfield Starter';
+$string['contactform'] = 'Formulaire de contact';
+$string['contactinfo'] = 'Contacter Brickfield';
+$string['coursecurriculum'] = 'Programme de formation';
+$string['coursesreviewed'] = 'Cours examinés';
+$string['demo'] = 'Démo';
+$string['disabled'] = 'Désactivé';
+$string['enabled'] = 'Activé';
+$string['enterprisedocs'] = 'Documentation plan Entreprise';
+$string['enterpriseinfo'] = 'Informations sur le kit d\'outils d\'entreprise';
+$string['enterprisetoolkit'] = 'Boîte à outils Accessibillity+';
+$string['facebook'] = 'Sur Facebook';
+$string['instagram'] = 'Sur Instagram';
+$string['lettersignup'] = 'Inscription à la newsletter';
+$string['linkedin'] = 'Lien LinkedIn';
+$string['newsletter'] = 'Bulletin';
+$string['notinstalled'] = 'Non installé';
+$string['pending'] = 'En attente';
+$string['pluginname'] = 'Vue d\'ensemble de l\'accessibilité';
+$string['privacy:nullproviderreason'] = 'Le plugin Vue d\'ensemble de l\'accessibilité n\'enregistre aucune donnée personnelle.';
+$string['registered'] = 'Enregistré';
+$string['reviewblock'] = 'Bloc Rapport d\'accessibilité';
+$string['reviewplusblock'] = 'Bloc Rapport d\'accessibilité+';
+$string['starterdocs'] = 'Documention du plan Starter';
+$string['startertoolkit'] = 'Boîte à outils d\'accessibilité';
+$string['status'] = 'Statut actuel';
+$string['toolkitunregistered'] = 'Boîte à outils non enregistrée';
+$string['training'] = 'Aperçu de la formation';
+$string['pending'] = 'Pending';
+$string['twitter'] = 'X (ex Twitter)';
+$string['unregistered'] = 'Non enregistré';
